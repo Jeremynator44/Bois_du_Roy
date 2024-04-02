f36f785375329f89f6f6175327c10d36c097f38a
Jérémy Loubeyre <jeremy.loubeyre44@gmail.com>
2024-04-02 17:24:07 +0200

    Fin visuel  Form Connexion (2eme moitié AP).

--- Bois du Rois/Connexion.Designer.cs
+++ Bois du Rois/Connexion.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Connexion));
             this.textBoxLogin = new System.Windows.Forms.TextBox();
             this.textBoxMdp = new System.Windows.Forms.TextBox();
             this.btnConnexion = new System.Windows.Forms.Button();
@@ -36,6 +37,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.lbl_entete = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBoxLogin
@@ -44,8 +47,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxLogin.Location = new System.Drawing.Point(346, 228);
+            this.textBoxLogin.MaxLength = 5;
             this.textBoxLogin.Name = "textBoxLogin";
-            this.textBoxLogin.Size = new System.Drawing.Size(271, 20);
+            this.textBoxLogin.Size = new System.Drawing.Size(285, 20);
             this.textBoxLogin.TabIndex = 0;
             // 
             // textBoxMdp
@@ -55,7 +59,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxMdp.Location = new System.Drawing.Point(346, 291);
             this.textBoxMdp.Name = "textBoxMdp";
-            this.textBoxMdp.Size = new System.Drawing.Size(271, 20);
+            this.textBoxMdp.Size = new System.Drawing.Size(285, 20);
             this.textBoxMdp.TabIndex = 1;
             this.textBoxMdp.UseSystemPasswordChar = true;
             // 
@@ -104,7 +108,7 @@
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(362, 141);
+            this.label1.Location = new System.Drawing.Point(370, 144);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(218, 42);
             this.label1.TabIndex = 5;
@@ -115,7 +119,7 @@
             this.lbl_entete.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.lbl_entete.Location = new System.Drawing.Point(0, 0);
             this.lbl_entete.Name = "lbl_entete";
-            this.lbl_entete.Size = new System.Drawing.Size(1829, 72);
+            this.lbl_entete.Size = new System.Drawing.Size(980, 72);
             this.lbl_entete.TabIndex = 6;
             // 
             // label2
@@ -129,12 +133,24 @@
             this.label2.TabIndex = 7;
             this.label2.Text = "Bois du Roy";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(231, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 72);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
             // Connexion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(966, 480);
+            this.ClientSize = new System.Drawing.Size(980, 496);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lbl_entete);
             this.Controls.Add(this.label1);
@@ -145,6 +161,7 @@
             this.Controls.Add(this.textBoxLogin);
             this.Name = "Connexion";
             this.Text = "Connexion";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,6 +177,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lbl_entete;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
