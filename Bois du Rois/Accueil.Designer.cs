ce1531a87d3b4bac61ed2ba3065ab5a55e719e89
Jeremy Loubeyre <jeremy.loubeyre44@gmail.com>
2024-04-09 00:37:38 +0200

    Continuation Form_Accueil

--- Bois du Rois/Accueil.Designer.cs
+++ Bois du Rois/Accueil.Designer.cs
@@ -28,19 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Accueil));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_entete = new System.Windows.Forms.Label();
+            this.btn_NDF_Employés = new System.Windows.Forms.Button();
+            this.btn_Liste_Employés = new System.Windows.Forms.Button();
+            this.btn_Liste_Produits = new System.Windows.Forms.Button();
+            this.btn_Compte = new System.Windows.Forms.Button();
+            this.btn_Accueil = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(258, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(123, 89);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_entete
+            // 
+            this.lbl_entete.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lbl_entete.Location = new System.Drawing.Point(0, 0);
+            this.lbl_entete.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_entete.Name = "lbl_entete";
+            this.lbl_entete.Size = new System.Drawing.Size(1307, 89);
+            this.lbl_entete.TabIndex = 9;
+            // 
+            // btn_NDF_Employés
+            // 
+            this.btn_NDF_Employés.BackColor = System.Drawing.Color.DimGray;
+            this.btn_NDF_Employés.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btn_NDF_Employés.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_NDF_Employés.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_NDF_Employés.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_NDF_Employés.Location = new System.Drawing.Point(448, 0);
+            this.btn_NDF_Employés.Name = "btn_NDF_Employés";
+            this.btn_NDF_Employés.Size = new System.Drawing.Size(199, 89);
+            this.btn_NDF_Employés.TabIndex = 18;
+            this.btn_NDF_Employés.Text = "NDF Employés";
+            this.btn_NDF_Employés.UseVisualStyleBackColor = false;
+            // 
+            // btn_Liste_Employés
+            // 
+            this.btn_Liste_Employés.BackColor = System.Drawing.Color.DimGray;
+            this.btn_Liste_Employés.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btn_Liste_Employés.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Liste_Employés.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Liste_Employés.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Liste_Employés.Location = new System.Drawing.Point(671, 0);
+            this.btn_Liste_Employés.Name = "btn_Liste_Employés";
+            this.btn_Liste_Employés.Size = new System.Drawing.Size(199, 89);
+            this.btn_Liste_Employés.TabIndex = 19;
+            this.btn_Liste_Employés.Text = "Liste Employés";
+            this.btn_Liste_Employés.UseVisualStyleBackColor = false;
+            // 
+            // btn_Liste_Produits
+            // 
+            this.btn_Liste_Produits.BackColor = System.Drawing.Color.DimGray;
+            this.btn_Liste_Produits.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btn_Liste_Produits.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Liste_Produits.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Liste_Produits.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Liste_Produits.Location = new System.Drawing.Point(892, 0);
+            this.btn_Liste_Produits.Name = "btn_Liste_Produits";
+            this.btn_Liste_Produits.Size = new System.Drawing.Size(199, 89);
+            this.btn_Liste_Produits.TabIndex = 20;
+            this.btn_Liste_Produits.Text = "Liste Produits";
+            this.btn_Liste_Produits.UseVisualStyleBackColor = false;
+            // 
+            // btn_Compte
+            // 
+            this.btn_Compte.BackColor = System.Drawing.Color.Black;
+            this.btn_Compte.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_Compte.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Compte.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Compte.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Compte.Location = new System.Drawing.Point(1108, 0);
+            this.btn_Compte.Name = "btn_Compte";
+            this.btn_Compte.Size = new System.Drawing.Size(199, 89);
+            this.btn_Compte.TabIndex = 21;
+            this.btn_Compte.Text = "Jean-Durand";
+            this.btn_Compte.UseVisualStyleBackColor = false;
+            // 
+            // btn_Accueil
+            // 
+            this.btn_Accueil.BackColor = System.Drawing.Color.DimGray;
+            this.btn_Accueil.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btn_Accueil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Accueil.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Accueil.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Accueil.Location = new System.Drawing.Point(31, 0);
+            this.btn_Accueil.Name = "btn_Accueil";
+            this.btn_Accueil.Size = new System.Drawing.Size(256, 89);
+            this.btn_Accueil.TabIndex = 22;
+            this.btn_Accueil.Text = "Bois du Roy";
+            this.btn_Accueil.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(302, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "label1";
             // 
             // Accueil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1307, 610);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Accueil);
+            this.Controls.Add(this.btn_Compte);
+            this.Controls.Add(this.btn_Liste_Produits);
+            this.Controls.Add(this.btn_Liste_Employés);
+            this.Controls.Add(this.btn_NDF_Employés);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbl_entete);
             this.Name = "Accueil";
             this.Text = "Accueil";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_entete;
+        private System.Windows.Forms.Button btn_NDF_Employés;
+        private System.Windows.Forms.Button btn_Liste_Employés;
+        private System.Windows.Forms.Button btn_Liste_Produits;
+        private System.Windows.Forms.Button btn_Compte;
+        private System.Windows.Forms.Button btn_Accueil;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Bois du Rois/Connexion.Designer.cs
+++ Bois du Rois/Connexion.Designer.cs
@@ -35,9 +35,9 @@
             this.lbl_login = new System.Windows.Forms.Label();
             this.lbl_mdp = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.lbl_entete = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_entete = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,10 +46,11 @@
             this.textBoxLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxLogin.Location = new System.Drawing.Point(346, 228);
+            this.textBoxLogin.Location = new System.Drawing.Point(461, 281);
+            this.textBoxLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxLogin.MaxLength = 5;
             this.textBoxLogin.Name = "textBoxLogin";
-            this.textBoxLogin.Size = new System.Drawing.Size(285, 20);
+            this.textBoxLogin.Size = new System.Drawing.Size(379, 22);
             this.textBoxLogin.TabIndex = 0;
             // 
             // textBoxMdp
@@ -57,9 +58,10 @@
             this.textBoxMdp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxMdp.Location = new System.Drawing.Point(346, 291);
+            this.textBoxMdp.Location = new System.Drawing.Point(461, 358);
+            this.textBoxMdp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxMdp.Name = "textBoxMdp";
-            this.textBoxMdp.Size = new System.Drawing.Size(285, 20);
+            this.textBoxMdp.Size = new System.Drawing.Size(379, 22);
             this.textBoxMdp.TabIndex = 1;
             this.textBoxMdp.UseSystemPasswordChar = true;
             // 
@@ -74,9 +76,10 @@
             this.btnConnexion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnConnexion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnConnexion.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.btnConnexion.Location = new System.Drawing.Point(430, 345);
+            this.btnConnexion.Location = new System.Drawing.Point(573, 425);
+            this.btnConnexion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnConnexion.Name = "btnConnexion";
-            this.btnConnexion.Size = new System.Drawing.Size(98, 39);
+            this.btnConnexion.Size = new System.Drawing.Size(131, 48);
             this.btnConnexion.TabIndex = 2;
             this.btnConnexion.Text = "Connexion";
             this.btnConnexion.UseVisualStyleBackColor = false;
@@ -87,9 +90,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_login.AutoSize = true;
-            this.lbl_login.Location = new System.Drawing.Point(272, 231);
+            this.lbl_login.Location = new System.Drawing.Point(363, 284);
+            this.lbl_login.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_login.Name = "lbl_login";
-            this.lbl_login.Size = new System.Drawing.Size(59, 13);
+            this.lbl_login.Size = new System.Drawing.Size(69, 16);
             this.lbl_login.TabIndex = 3;
             this.lbl_login.Text = "Identifiant :";
             // 
@@ -99,59 +103,67 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_mdp.AutoSize = true;
-            this.lbl_mdp.Location = new System.Drawing.Point(262, 294);
+            this.lbl_mdp.Location = new System.Drawing.Point(349, 362);
+            this.lbl_mdp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_mdp.Name = "lbl_mdp";
-            this.lbl_mdp.Size = new System.Drawing.Size(78, 13);
+            this.lbl_mdp.Size = new System.Drawing.Size(96, 16);
             this.lbl_mdp.TabIndex = 4;
             this.lbl_mdp.Text = "Mot de Passe :";
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(370, 144);
+            this.label1.Location = new System.Drawing.Point(493, 177);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(218, 42);
+            this.label1.Size = new System.Drawing.Size(291, 52);
             this.label1.TabIndex = 5;
             this.label1.Text = "Se connecter";
             // 
-            // lbl_entete
+            // button5
             // 
-            this.lbl_entete.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.lbl_entete.Location = new System.Drawing.Point(0, 0);
-            this.lbl_entete.Name = "lbl_entete";
-            this.lbl_entete.Size = new System.Drawing.Size(980, 72);
-            this.lbl_entete.TabIndex = 6;
-            // 
-            // label2
-            // 
-            this.label2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(38, 18);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(218, 42);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Bois du Roy";
+            this.button5.BackColor = System.Drawing.Color.DimGray;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Location = new System.Drawing.Point(31, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(256, 89);
+            this.button5.TabIndex = 25;
+            this.button5.Text = "Bois du Roy";
+            this.button5.UseVisualStyleBackColor = false;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.DimGray;
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(231, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(258, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 72);
-            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.Size = new System.Drawing.Size(123, 89);
+            this.pictureBox1.TabIndex = 24;
             this.pictureBox1.TabStop = false;
+            // 
+            // lbl_entete
+            // 
+            this.lbl_entete.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lbl_entete.Location = new System.Drawing.Point(0, 0);
+            this.lbl_entete.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_entete.Name = "lbl_entete";
+            this.lbl_entete.Size = new System.Drawing.Size(1307, 89);
+            this.lbl_entete.TabIndex = 23;
             // 
             // Connexion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(980, 496);
+            this.ClientSize = new System.Drawing.Size(1307, 610);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.lbl_entete);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lbl_mdp);
@@ -159,6 +171,7 @@
             this.Controls.Add(this.btnConnexion);
             this.Controls.Add(this.textBoxMdp);
             this.Controls.Add(this.textBoxLogin);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Connexion";
             this.Text = "Connexion";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -175,9 +188,9 @@
         private System.Windows.Forms.Label lbl_login;
         private System.Windows.Forms.Label lbl_mdp;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lbl_entete;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_entete;
     }
 }
 
